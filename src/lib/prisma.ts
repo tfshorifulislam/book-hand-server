2c92a8a5c6bd2c11c32eaa8cbf7d850384fb3f36
tfshorifulislam <tfshorifulislam@gmail.com>
2026-09-03 00:22:57 +0600

    first commit

---
+++ src/lib/prisma.ts
@@ -0,0 +1,24 @@
+import "dotenv/config";
+
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client.js";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
