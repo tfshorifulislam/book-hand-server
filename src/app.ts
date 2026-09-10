2069b7bab505c5ee17a2d8f1d83c6a7a1f70f889
tfshorifulislam <tfshorifulislam@gmail.com>
2026-09-10 15:16:59 +0600

    profile userId route create done

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import homeRouter from './Routes/home.route.js';
 import booksRouter from './Routes/books.route.js';
+import userIdProfile from './Routes/userId.Profile.Route.js';
 
 
 const app = express();
@@ -13,5 +14,8 @@ app.use("/", homeRouter);
 
 //get books route;
 app.use('/api', booksRouter);
+
+//get user profile;
+app.use('/api' , userIdProfile)
 
 export default app;
