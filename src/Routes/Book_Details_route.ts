a32b0247620e7c55588d90b7506b337f4e9f1ec6
tfshorifulislam <tfshorifulislam@gmail.com>
2026-09-14 00:47:37 +0600

    user profile book find service add done

--- src/Routes/Book_Details_route.ts
+++ src/Routes/Book_Details_route.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import verifyToken from "../middleWare/verify.token.js";
-import { bookDetsilsController } from "../Controllers/book.Details.Controller.js";
+import { bookDetailsController } from "../Controllers/book.Details.Controller.js";
 
 const router = Router();
 
-router.post("/book-details/:id", verifyToken, bookDetsilsController);
+router.get("/book-details/:id", verifyToken, bookDetailsController);
 
 export default router;
