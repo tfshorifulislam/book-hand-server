5feea3b211fd077fbed42384226ccbfe18da80b0
tfshorifulislam <tfshorifulislam@gmail.com>
2026-09-18 01:08:15 +0600

    redis remove from save post

--- src/Services/books.Save.Service.ts
+++ src/Services/books.Save.Service.ts
@@ -1,15 +1,15 @@
-import redis from "../config/redis.js";
 import { prisma } from "../lib/prisma.js";
 
+export const saveBookService = async (
+    userId: string,
+    listingId: string
+) => {
+    const savedBook = await prisma.savedBook.create({
+        data: {
+            userId,
+            listingId,
+        },
+    });
 
-export const saveBookService = async ( userId: string, listingId: string) => {
-  const savedBook = await prisma.savedBook.create({ data: { userId, listingId, }, });
-
-  const cacheKey = `saved:${userId}:${listingId}`;
-
-  await redis.set(cacheKey, "1", {
-    EX: 60 * 5,
-  });
-
-  return savedBook;
+    return savedBook;
 };
