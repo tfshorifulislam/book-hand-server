bebfcafbb92ab8c4b163b35d695b3552e35142ec
tfshorifulislam <tfshorifulislam@gmail.com>
2026-09-17 08:14:46 +0600

    user post controller add redis

--- src/Controllers/user.Post.Controller.ts
+++ src/Controllers/user.Post.Controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import { getUserBooksService } from "../Services/uses.Post.Service.js";
+import redis from "../config/redis.js";
 
 export const getUserBooks = async (
   req: Request,
@@ -25,11 +26,39 @@ export const getUserBooks = async (
       50
     );
 
+    const cacheKey = `profile:books:userId=${userId}:page=${page}:limit=${limit}`;
+    const cachedData = await redis.get(cacheKey);
+
+    if (cachedData) {
+      console.log("PROFILE BOOKS CACHE HIT:", cacheKey);
+
+      const result = JSON.parse(cachedData);
+
+      return res.status(200).json({
+        success: true,
+        ...result,
+      });
+    }
+
+    console.log("PROFILE BOOKS CACHE MISS:", cacheKey);
+
+
     const result = await getUserBooksService(
       userId,
       page,
       limit
     );
+
+    // Save to Redis for 5 minutes
+    await redis.set(
+      cacheKey,
+      JSON.stringify(result),
+      {
+        EX: 300,
+      }
+    );
+
+    console.log("PROFILE BOOKS CACHE SAVED:", cacheKey);
 
     return res.status(200).json({
       success: true,
