6809abe24fc07302c42de138108ab1d1a0ee229a
tfshorifulislam <tfshorifulislam@gmail.com>
2026-09-21 10:30:49 +0600

    books save service update

--- src/Services/books.Save.Service.ts
+++ src/Services/books.Save.Service.ts
@@ -2,21 +2,21 @@ import { prisma } from "../lib/prisma.js";
 import redis from "../config/redis.js";
 
 export const saveBookService = async (
-    userId: string,
-    listingId: string
+  userId: string,
+  listingId: string
 ) => {
-    const savedBook = await prisma.savedBook.create({
-        data: {
-            userId,
-            listingId,
-        },
-    });
+  const savedBook = await prisma.savedBook.create({
+    data: {
+      userId,
+      listingId,
+    },
+  });
 
-    const keys = await redis.keys(`books:user=${userId}:*`);
+  const keys = await redis.keys(`books:user=${userId}:*`);
 
-    if (keys.length > 0) {
-        await redis.del(keys);
-    }
+  if (keys.length > 0) {
+    await redis.del(...keys);
+  }
 
-    return savedBook;
+  return savedBook;
 };
