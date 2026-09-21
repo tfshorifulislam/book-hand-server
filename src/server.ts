cd8241acf2b45ff9658fadb4ac854672c8560ca2
tfshorifulislam <tfshorifulislam@gmail.com>
2026-09-21 10:06:05 +0600

    server update

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,7 @@
 import "dotenv/config";
 
-
 import app from "./app.js";
-import { connectRedis } from "./config/redis.js";
 
-await connectRedis();
 export default app;
 
 const PORT = process.env.PORT || 8000;
